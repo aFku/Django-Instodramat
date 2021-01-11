2d9d4edb0c50686aed466319a8f505f2a3f91cf9
aFku <zapasowyrowerek@gmail.com>
2021-01-11 19:26:01 +0100

    Added Login/Logout View + Login template | Added 2 variable in settings

--- Instodramat/Users/urls.py
+++ Instodramat/Users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 #path('url', '<view name>', name=<''url name>)
 urlpatterns = [
-    path('profile/<int:pk>', views.ProfileView.as_view(), name='xd'),
+    path('profile/<int:pk>', views.ProfileView.as_view(), name='profile'),
     path('registry', views.user_create_view, name='registry'),
+    path('login', views.CustomLoginView.as_view(), name='login'),
+    path('logout', views.CustomLogoutView.as_view(), name='logout')
 ]
--- Instodramat/Users/views.py
+++ Instodramat/Users/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import ProfileCreationForm, EmailUserCreationForm
 from .models import Profile
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 
+# -------------------------- Class-based
 
 
 class ProfileView(DetailView):
@@ -30,7 +33,27 @@ class ProfileView(DetailView):
         #context['photos'] = self.photos
         return context
 
-# -----------------------------------------------------------------------------------------------------------------
+
+class CustomLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'login.html'
+    # Redirect users already logged in
+    redirect_authenticated_user = True
+
+    success_message = 'Logged in'
+
+
+class CustomLogoutView(LogoutView):
+    """
+    This view just add message after log out
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.success(request, 'Successfully logged out')
+        return response
+
+# -------------------------- Method-based
+
 
 def user_create_view(request):
     if not request.user.is_authenticated:
@@ -39,14 +62,17 @@ def user_create_view(request):
             profileform = ProfileCreationForm(request.POST)
             if userform.is_valid() and profileform.is_valid():
                 # Create here two objects, and connect them.
-                new_user = userform.save()
+                # form.save() is not hashing password, so use create_user()
+                new_user = User.objects.create_user(username=userform.cleaned_data['username'],
+                                                    password=userform.cleaned_data['password1'],
+                                                    email=userform.cleaned_data['email'])
                 # Create profile
                 new_profile = profileform.save()
                 # Connect user with profile with one to one field
                 new_profile.user = new_user
                 new_profile.save()
                 messages.success(request, 'Account has been created successfully')
-                return redirect(f'profile/{new_user.id}')
+                return redirect('login')
             else:
                 messages.error(request, 'Account cannot be created')
         else:
@@ -55,7 +81,6 @@ def user_create_view(request):
         return render(request, 'registry.html', {'user_form': userform, 'profile_form': profileform})
     #### For a moment
     return HttpResponseNotFound('temporary redirection')
-
 
 
 
