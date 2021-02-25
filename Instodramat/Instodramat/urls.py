fe37ec8494912ac0c13db479b296d10d6bcfedea
aFku <zapasowyrowerek@gmail.com>
2021-02-25 22:47:35 +0100

    Fix v17

--- Instodramat/Instodramat/urls.py
+++ Instodramat/Instodramat/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from Images.views import temporary_mainpage_view
+from Images import views
 
 urlpatterns = [
     path('profile/', include('Users.urls')),
     path('photos/', include('Images.urls')),
-    path('/', temporary_mainpage_view, name='mainpage')
+    path('/', views.temporary_mainpage_view, name='mainpage')
 ]
