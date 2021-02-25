f124f77c9e094666cacb33576f4f2a4d063018f9
aFku <zapasowyrowerek@gmail.com>
2021-02-25 22:15:59 +0100

    Fix v14

--- Instodramat/Instodramat/settings.py
+++ Instodramat/Instodramat/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/collect/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/collect')
 
 MEDIA_URL = '/media/'
