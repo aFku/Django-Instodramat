c7f75e6972ecdd93c2885be68d0107cfe78ba275
aFku <zapasowyrowerek@gmail.com>
2021-02-25 21:46:07 +0100

    Fix v7

--- Instodramat/Instodramat/wsgi.py
+++ Instodramat/Instodramat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instodramat.Instodramat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instodramat.settings')
 
 application = get_wsgi_application()
