11053dcaba0ea2c9ae613daa5747ef13b01e517e
aFku <zapasowyrowerek@gmail.com>
2021-10-14 14:03:23 +0200

    Fix URL registry -> register

--- Instodramat/Users/urls.py
+++ Instodramat/Users/urls.py
@@ -5,7 +5,7 @@ from . import views
 #path('url', '<view name>', name=<''url name>)
 urlpatterns = [
     path('<int:pk>/', views.ProfileView.as_view(), name='profile'),
-    path('registry/', views.user_create_view, name='registry'),
+    path('register/', views.user_create_view, name='registry'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('<int:pk>/follow/', views.follow_ajax, name='follow_ajax'),
